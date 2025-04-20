ee7a16b7af95e3ec27749c64a621098ddffbf8c7
Henry Post <HenryFBP@gmail.com>
2025-04-20 15:56:24 -0500

    try to add block loot tables

--- src/main/java/io/meltingscales/amethystshards/block/ModBlocks.java
+++ src/main/java/io/meltingscales/amethystshards/block/ModBlocks.java
@@ -3,11 +3,16 @@ package io.meltingscales.amethystshards.block;
 import io.meltingscales.amethystshards.AmethystShards;
 import io.meltingscales.amethystshards.item.ModItems;
 import java.util.function.Supplier;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -19,30 +24,64 @@ public class ModBlocks {
 
   public static final RegistryObject<Block> ALEXANDRITE_BLOCK =
       registerBlock(
-          "alexandrite_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)));
+          "alexandrite_block",
+          () ->
+              new Block(
+                  BlockBehaviour.Properties.of()
+                      .mapColor(MapColor.METAL)
+                      .instrument(NoteBlockInstrument.IRON_XYLOPHONE)
+                      .requiresCorrectToolForDrops()
+                      .strength(5.0F, 6.0F)
+                      .sound(SoundType.METAL)));
 
   public static final RegistryObject<Block> RAW_ALEXANDRITE_BLOCK =
       registerBlock(
           "raw_alexandrite_block",
-          () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)));
+          () ->
+              new Block(
+                  BlockBehaviour.Properties.of()
+                      .mapColor(MapColor.METAL)
+                      .instrument(NoteBlockInstrument.IRON_XYLOPHONE)
+                      .requiresCorrectToolForDrops()
+                      .strength(5.0F, 6.0F)
+                      .sound(SoundType.METAL)));
 
   public static final RegistryObject<Block> ALEXANDRITE_ORE =
       registerBlock(
-          "alexandrite_ore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
-
+          "alexandrite_ore",
+          () ->
+              new DropExperienceBlock(
+                  BlockBehaviour.Properties.copy(Blocks.STONE)
+                      .strength(5f)
+                      .requiresCorrectToolForDrops(),
+                  UniformInt.of(2, 5)));
   public static final RegistryObject<Block> DEEPSLATE_ALEXANDRITE_ORE =
       registerBlock(
           "deepslate_alexandrite_ore",
-          () -> new Block(BlockBehaviour.Properties.copy(ALEXANDRITE_ORE.get())));
-
+          () ->
+              new DropExperienceBlock(
+                  BlockBehaviour.Properties.copy(Blocks.DEEPSLATE)
+                      .strength(5f)
+                      .requiresCorrectToolForDrops(),
+                  UniformInt.of(3, 7)));
   public static final RegistryObject<Block> END_STONE_ALEXANDRITE_ORE =
       registerBlock(
           "end_stone_alexandrite_ore",
-          () -> new Block(BlockBehaviour.Properties.copy(ALEXANDRITE_ORE.get())));
+          () ->
+              new DropExperienceBlock(
+                  BlockBehaviour.Properties.copy(Blocks.END_STONE)
+                      .strength(5f)
+                      .requiresCorrectToolForDrops(),
+                  UniformInt.of(5, 8)));
   public static final RegistryObject<Block> NETHER_ALEXANDRITE_ORE =
       registerBlock(
           "nether_alexandrite_ore",
-          () -> new Block(BlockBehaviour.Properties.copy(ALEXANDRITE_ORE.get())));
+          () ->
+              new DropExperienceBlock(
+                  BlockBehaviour.Properties.copy(Blocks.NETHERRACK)
+                      .strength(5f)
+                      .requiresCorrectToolForDrops(),
+                  UniformInt.of(3, 6)));
 
   private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
     RegistryObject<T> toReturn = BLOCKS.register(name, block);
@@ -50,13 +89,12 @@ public class ModBlocks {
     return toReturn;
   }
 
-  public static <T extends Block> RegistryObject<Item> registerBlockItem(
+  private static <T extends Block> RegistryObject<Item> registerBlockItem(
       String name, RegistryObject<T> block) {
     return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
   }
 
   public static void register(IEventBus eventBus) {
-
     BLOCKS.register(eventBus);
   }
 }

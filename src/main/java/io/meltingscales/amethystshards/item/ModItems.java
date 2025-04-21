7c559a5d1da63b4ad72af9ee888dfb7ed61cb9ef
Henry Post <HenryFBP@gmail.com>
2025-04-20 20:09:15 -0500

    get my name working

---
+++ src/main/java/io/meltingscales/amethystshards/item/ModItems.java
@@ -0,0 +1,22 @@
+package io.meltingscales.amethystshards.item;
+
+import io.meltingscales.amethystshards.AmethystShardsMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+  public static final DeferredRegister<Item> ITEMS =
+      DeferredRegister.create(ForgeRegistries.ITEMS, AmethystShardsMod.MOD_ID);
+
+  public static final RegistryObject<Item> ALEXANDRITE =
+      ITEMS.register("alexandrite", () -> new Item(new Item.Properties()));
+  public static final RegistryObject<Item> RAW_ALEXANDRITE =
+      ITEMS.register("raw_alexandrite", () -> new Item(new Item.Properties()));
+
+  public static void register(IEventBus eventBus) {
+    ITEMS.register(eventBus);
+  }
+}
